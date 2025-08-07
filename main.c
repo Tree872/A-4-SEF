2b1e32fb9bcb30a6f000c0361612dce4e7e09cec
Tri <tphan7231@conestogac.on.ca>
2025-08-07 11:50:13 -0400

    Cleaned up print functions

--- main.c
+++ main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "Validation.h"
 #include "FileIO.h"
 #include "Customer.h"
@@ -11,41 +12,45 @@ void printPart(const Part* p);
 void printOrder(const Order* order);
 
 int main() {
-  Customer customers[10]; 
-  Part parts[10]; 
+  Customer *customers = (Customer*)malloc(CUSTOMERS_LIMIT * sizeof(Customer)); 
+  Part* parts = (Part*)malloc(PARTS_LIMIT * sizeof(Part));
+  Order* orders = (Order*)malloc(ORDERS_LIMIT * sizeof(Order));
   int customerCount = loadCustomers(customers, CUSTOMERS_FILE);
   int partCount = loadParts(parts, PARTS_FILE);
   // Order must be loaded after customers and parts
-  Order orders[10];
   int orderCount = loadOrders(orders, parts, partCount, customers, customerCount, ORDERS_FILE);
   printOrder(&orders[0]);
   
+  free(customers);
+  free(parts);
+  free(orders);
 }
 void printCustomer(const Customer* customer) {
-  printf("Customer Information:\n");
   printf("---------------------\n");
-  printf("Name: %s\n", customer->customerName);
-  printf("Address: %s\n", customer->customerAddress);
-  printf("City: %s\n", customer->customerCity);
-  printf("Province: %s\n", customer->customerProvince);
-  printf("Postal Code: %s\n", customer->customerPostalCode);
-  printf("Phone: %s\n", customer->customerPhone);
-  printf("Email: %s\n", customer->customerEmail);
-  printf("Customer ID: %d\n", customer->customerID);
-  printf("Credit Limit: %.2f\n", customer->customerCreditLimit);
-  printf("Account Balance: %.2f\n", customer->currentAccountBalance);
-  printf("Last Payment Made: %s\n",
-    (customer->lastPaymentMade[0] != '\0') ? customer->lastPaymentMade : "N/A");
-  printf("Join Date: %s\n", customer->customerJoinDate);
-  printf("\n");
+  printf("Name             : %s\n", customer->customerName);
+  printf("Address          : %s\n", customer->customerAddress);
+  printf("City             : %s\n", customer->customerCity);
+  printf("Province         : %s\n", customer->customerProvince);
+  printf("Postal Code      : %s\n", customer->customerPostalCode);
+  printf("Phone            : %s\n", customer->customerPhone);
+  printf("Email            : %s\n", customer->customerEmail);
+  printf("Customer ID      : %d\n", customer->customerID);
+  printf("Credit Limit     : %.2f\n", customer->customerCreditLimit);
+  printf("Account Balance  : %.2f\n", customer->currentAccountBalance);
+  if (customer->lastPaymentMade[0] != '\0')
+    printf("Last Payment Made: %s\n", customer->lastPaymentMade);
+  else {
+    printf("Last Payment Made: %s\n", "N/A");
+  }
+  printf("Join Date        : %s\n", customer->customerJoinDate);
+  printf("---------------------\n");
 }
 void printPart(const Part* p) {
   if (p == NULL) {
     printf("Invalid Part\n");
     return;
   }
-
-  printf("----- Part Information -----\n");
+  printf("---------------------\n");
   printf("Part ID        : %d\n", p->partID);
   printf("Name           : %s\n", p->partName);
   printf("Number         : %s\n", p->partNumber);
@@ -67,7 +72,7 @@ void printPart(const Part* p) {
     printf("Unknown Status (%d)\n", p->partStatus);
   }
 
-  printf("-----------------------------\n");
+  printf("---------------------\n");
 }
 
 void printOrder(const Order* order) {
@@ -75,7 +80,7 @@ void printOrder(const Order* order) {
     printf("Invalid Order\n");
     return;
   }
-  printf("----- Order Information -----\n");
+  printf("---------------------\n");
   printf("Order ID      : %lld\n", order->orderID);
   printf("Order Date    : %s\n", order->orderDate);
   printf("Status        : %d\n", order->orderStatus);
@@ -87,5 +92,5 @@ void printOrder(const Order* order) {
     printf("Part ID: %d, Quantity Ordered: %d\n",
            order->orderedParts[i].partID, order->orderedParts[i].quantityOrdered);
   }
-  printf("-----------------------------\n");
+  printf("---------------------\n");
 }
